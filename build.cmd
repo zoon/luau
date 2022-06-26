69000a9152f42970877e3017e0bef1401b00e706
Andrew Zhilin <andrew_zhilin@yahoo.com>
2022-06-26 15:49:40 +0300

    update build script

--- build.cmd
+++ build.cmd
@@ -3,3 +3,5 @@ cd cmake
 cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo
 cmake --build . --target Luau.Repl.CLI --config RelWithDebInfo
 cmake --build . --target Luau.Analyze.CLI --config RelWithDebInfo
+cp RelWithDebInfo\luau.exe C:\Lua\bin\luau-src\luau.exe
+cp RelWithDebInfo\luau-analyze.exe C:\Lua\bin\luau-src\luau-analyze.exe
