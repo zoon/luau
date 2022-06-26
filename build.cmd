f50e0a33cf4a44e819f71322a2f638740ac6ca80
Andrew Zhilin <andrew_zhilin@yahoo.com>
2022-06-26 15:25:28 +0300

    add build script

---
+++ build.cmd
@@ -0,0 +1,5 @@
+REM mkdir cmake
+cd cmake
+cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo
+cmake --build . --target Luau.Repl.CLI --config RelWithDebInfo
+cmake --build . --target Luau.Analyze.CLI --config RelWithDebInfo
