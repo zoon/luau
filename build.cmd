4620a79dc965bc8d87ab7e743dac93cd16fc65ed
Andrew Zhilin <andrew_zhilin@yahoo.com>
2024-10-07 23:42:55 -0300

    update build (luau-ast)

--- build.cmd
+++ build.cmd
@@ -1,8 +1,8 @@
-REM mkdir cmake
+mkdir cmake
 cd cmake
 cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo
-cmake --build . --target Luau.Repl.CLI --config RelWithDebInfo
-cmake --build . --target Luau.Analyze.CLI --config RelWithDebInfo
+cmake --build . --target Luau.Repl.CLI Luau.Analyze.CLI Luau.Compile.CLI Luau.Ast.CLI --config RelWithDebInfo -j 2
 cp RelWithDebInfo\luau.exe C:\Lua\bin\luau-src\luau.exe
 cp RelWithDebInfo\luau-analyze.exe C:\Lua\bin\luau-src\luau-analyze.exe
+cp RelWithDebInfo\luau-ast.exe C:\Lua\bin\luau-src\luau-ast.exe
 cd ..
